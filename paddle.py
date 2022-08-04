c57eb53d186dfdc88004c6b0578e6e7ab183a742
Antony Quang <antony.quang@googlemail.com>
2022-08-04 21:49:02 +0100

    Add mouse controls

--- paddle.py
+++ paddle.py
@@ -10,6 +10,7 @@ class Paddle(Turtle):
         self.goto((0, -420))
         self.setheading(0)
         self.shapesize(stretch_wid=0.8, stretch_len=3)
+        self.ondrag(self.follow_mouse)
 
     def go_left(self):
         new_x = self.xcor() - 20
@@ -18,3 +19,9 @@ class Paddle(Turtle):
     def go_right(self):
         new_x = self.xcor() + 20
         self.goto(new_x, self.ycor())
+
+    def follow_mouse(self, x, y):
+        self.ondrag(None)
+        y = self.ycor()
+        self.goto(x, y)
+        self.ondrag(self.follow_mouse)
