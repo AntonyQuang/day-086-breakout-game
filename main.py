ad423b660241478127ca21f10cb3eb981872875f
Antony Quang <antony.quang@googlemail.com>
2022-08-04 21:54:03 +0100

    Remove bounce

--- main.py
+++ main.py
@@ -37,33 +37,5 @@ game_is_on = True
 while game_is_on:
     time.sleep(ball.move_speed)
     screen.update()
-    # Wall bounce
-    if ball.xcor() < -280 or ball.xcor() > 270:
-        ball.wall_bounce()
-
-    if ball.ycor() > 320:
-        ball.ceiling_bounce()
-
-    if abs(ball.xcor() - paddle.xcor()) < 40 and abs(ball.ycor() - paddle.ycor()) < 12 and ball.ycor() > -440:
-        ball.paddle_bounce()
-    # Determines how the ball will bounce if it hits a block
-    for block in blocks:
-        if abs(ball.xcor() - block.xcor()) < 40 and abs(ball.ycor() - block.ycor()) < 12:
-            # If the ball is moving to the right and hits the left side of the block, bounce horizontally
-            if (ball.xcor()-block.xcor()) > 25 and ball.x_move < 0:
-                ball.wall_bounce()
-            # If the ball is moving to the left and hits the right side of the block, bounce horizontally
-            if (ball.xcor()-block.xcor()) < -25 and ball.x_move > 0:
-                ball.wall_bounce()
-            # If the ball hits the bottom of the block, bounce vertically
-            if ball.ycor() - block.ycor() < -4 and ball.y_move > 0:
-                ball.ceiling_bounce()
-            # If the ball hits the top of the block, bounce vertically
-            if ball.ycor() - block.ycor() > 4 and ball.y_move < 0:
-                ball.ceiling_bounce()
-            block.goto(0, -500)
 
     ball.move()
-
-
-
