6111602beac44d6d314e1d4265fecca3fd3f1fe9
Antony Quang <antony.quang@googlemail.com>
2022-08-07 21:22:14 +0100

    Add behaviour when player wins or loses

--- main.py
+++ main.py
@@ -26,50 +26,78 @@ block_row_colors = ["#BF4B66",
                     "#C45E47"]
 columns = 8
 rows = len(block_row_colors)
-blocks = []
+blocks = {}
 for column in range(columns):
     x = -250 + 70*column
     for row in range(rows):
         y = 230 - 25*row
-        blocks.append(Block((x, y), block_row_colors[row]))
+        blocks[Block((x, y), block_row_colors[row])] = (x, y)
 ceiling = ScoreboardBorder()
 screen.listen()
 screen.update()
-time.sleep(1)
-game_is_on = True
-while game_is_on:
-    time.sleep(ball.move_speed)
-    screen.update()
-    # Wall bounce
-    if ball.xcor() < -280 or ball.xcor() > 270:
-        ball.wall_bounce()
+scoreboard.welcome()
 
-    if ball.ycor() > 320:
-        ball.ceiling_bounce()
 
-    if abs(ball.xcor() - paddle.xcor()) < 40 and abs(ball.ycor() - paddle.ycor()) < 12 and ball.ycor() > -440:
-        ball.paddle_bounce()
-    # Determines how the ball will bounce if it hits a block
+def play_game():
+    reset()
+    game_is_on = True
+    while game_is_on:
+        time.sleep(ball.move_speed)
+        screen.update()
+        # Wall bounce
+        if ball.xcor() < -280 or ball.xcor() > 270:
+            ball.wall_bounce()
+
+        if ball.ycor() > 320:
+            ball.ceiling_bounce()
+
+        if abs(ball.xcor() - paddle.xcor()) < 40 and abs(ball.ycor() - paddle.ycor()) < 12 and ball.ycor() > -440:
+            ball.paddle_bounce()
+        # Determines how the ball will bounce if it hits a block
+        for block in blocks:
+            if abs(ball.xcor() - block.xcor()) < 40 and abs(ball.ycor() - block.ycor()) < 12:
+                # If the ball is moving to the right and hits the left side of the block, bounce horizontally
+                if (ball.xcor()-block.xcor()) > 25 and ball.x_move < 0:
+                    ball.wall_bounce()
+                # If the ball is moving to the left and hits the right side of the block, bounce horizontally
+                if (ball.xcor()-block.xcor()) < -25 and ball.x_move > 0:
+                    ball.wall_bounce()
+                # If the ball hits the bottom of the block, bounce vertically
+                if ball.ycor() - block.ycor() < -4 and ball.y_move > 0:
+                    ball.ceiling_bounce()
+                # If the ball hits the top of the block, bounce vertically
+                if ball.ycor() - block.ycor() > 4 and ball.y_move < 0:
+                    ball.ceiling_bounce()
+                block.goto(0, -500)
+                scoreboard.point()
+
+
+        ball.move()
+
+        if ball.ycor() < -460:
+            scoreboard.game_over()
+            screen.onkey(play_game, "space")
+            screen.exitonclick()
+            game_is_on = False
+
+        if scoreboard.score == len(blocks)*100:
+            scoreboard.game_win()
+            screen.onkey(play_game, "space")
+            screen.exitonclick()
+            game_is_on = False
+
+
+def reset():
+    screen.onclick(None)
+    screen.onkey(None, "space")
+    scoreboard.reset()
+    # Put the blocks back to the starting positions
     for block in blocks:
-        if abs(ball.xcor() - block.xcor()) < 40 and abs(ball.ycor() - block.ycor()) < 12:
-            # If the ball is moving to the right and hits the left side of the block, bounce horizontally
-            if (ball.xcor()-block.xcor()) > 25 and ball.x_move < 0:
-                ball.wall_bounce()
-            # If the ball is moving to the left and hits the right side of the block, bounce horizontally
-            if (ball.xcor()-block.xcor()) < -25 and ball.x_move > 0:
-                ball.wall_bounce()
-            # If the ball hits the bottom of the block, bounce vertically
-            if ball.ycor() - block.ycor() < -4 and ball.y_move > 0:
-                ball.ceiling_bounce()
-            # If the ball hits the top of the block, bounce vertically
-            if ball.ycor() - block.ycor() > 4 and ball.y_move < 0:
-                ball.ceiling_bounce()
-            block.goto(0, -500)
-            scoreboard.point()
+        block.goto(blocks[block])
+    ball.goto(0, -200)
 
-    ball.move()
-    if ball.ycor() < -460:
-        game_is_on = False
-        time.sleep(1)
 
-screen.exitonclick()
+game_is_on = True
+screen.onkey(play_game, "space")
+
+screen.mainloop()
--- scoreboard.py
+++ scoreboard.py
@@ -9,17 +9,43 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.score = 0
-        self.score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(0, 340)
+        self.goto(0, 350)
         self.write(self.score, align="center", font=("Courier", 60, "normal"))
 
     def point(self):
         self.score +=100
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, -200)
+        self.write("GAME OVER", align="center", font=("Courier", 40, "normal"))
+        self.goto(0, -230)
+        self.write("Press Space to play again", align="center", font=("Courier", 20, "normal"))
+        self.goto(0, -260)
+        self.write("Click to exit", align="center", font=("Courier", 20, "normal"))
+
+    def game_win(self):
+        self.goto(0, -200)
+        self.write("YOU WIN!", align="center", font=("Courier", 40, "normal"))
+        self.goto(0, -230)
+        self.write("Press Space to play again", align="center", font=("Courier", 20, "normal"))
+        self.goto(0, -260)
+        self.write("Click to exit", align="center", font=("Courier", 20, "normal"))
+
+    def reset(self):
+        self.clear()
+        self.__init__()
+
+    def welcome(self):
+        self.goto(0, -150)
+        self.write("Press Space to Play", align="center", font=("Courier", 20, "normal"))
+        self.goto(0, -180)
+        self.write("Drag the paddle with your mouse", align="center", font=("Courier", 20, "normal"))
+
 
 class ScoreboardBorder(Turtle):
 
@@ -28,6 +54,6 @@ class ScoreboardBorder(Turtle):
         self.shape("square")
         self.color("white")
         self.penup()
-        self.goto((0, 320))
+        self.goto((0, 330))
         self.setheading(0)
         self.shapesize(stretch_wid=0.8, stretch_len=50)
